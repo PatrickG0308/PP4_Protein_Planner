6e8c8ba3c0d8c294c246d829c511778037444047
PatrickG0308 <>
2023-12-13 23:11:01 +0000

    Change Debug to True to troubleshoot Heroku issue

--- proteinplanner/settings.py
+++ proteinplanner/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "DEVELOPMENT" in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "proteinplanner-f5152efcdae3.herokuapp.com",
