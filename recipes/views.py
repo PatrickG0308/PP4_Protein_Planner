d6bce5abb83c01c646bc7e580a73c4519e1e3161
PatrickG0308 <>
2023-12-13 10:46:29 +0000

    Create CRUD function for recipes, create search, create homepage content

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,14 @@
-from django.views.generic import CreateView, ListView
+from django.views.generic import (
+    CreateView,
+    ListView,
+    DetailView,
+    DeleteView,
+    UpdateView
+)
 
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+
+from django.db.models import Q
 
 from .models import Recipe
 from .forms import RecipeForm
@@ -33,3 +41,33 @@ class AddRecipe(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(AddRecipe, self).form_valid(form)
+
+
+class RecipeDetail(DetailView):
+    """View a single recipe"""
+
+    template_name = "recipes/recipe_detail.html"
+    model = Recipe
+    context_object_name = "recipe"
+
+
+class EditRecipe(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """Edit a recipe"""
+
+    template_name = "recipes/edit_recipe.html"
+    model = Recipe
+    form_class = RecipeForm
+    success_url = "/recipes/"
+
+    def test_func(self):
+        return self.request.user == self.get_object().user
+
+
+class DeleteRecipe(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """Delete a recipe"""
+
+    model = Recipe
+    success_url = "/recipes/"
+
+    def test_func(self):
+        return self.request.user == self.get_object().user
