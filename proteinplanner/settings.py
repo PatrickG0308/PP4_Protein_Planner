c93eacaf2b7181c0602b4c871d72c3bfa0285b63
PatrickG0308 <>
2023-12-13 22:01:50 +0000

    Create development debug

--- proteinplanner/settings.py
+++ proteinplanner/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = [
     "proteinplanner-f5152efcdae3.herokuapp.com",
